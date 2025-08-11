4e9c0e313ea0c6f7a259a53a1c359fdbaf156b2c
Andres Velasco <afvelasco@gmai.com>
2025-08-11 11:36:51 -0500

    Se agrega enunciado para el archivo clientes.py

---
+++ clientes.py
@@ -0,0 +1,6 @@
+"""
+Se trata de escribir un programa con un CRUD para clientes
+Cada producto debe tener los siguientes datos:
+Identificación (string), Nombre (string), saldo (float), 
+cupo_crédito (float)
+"""
